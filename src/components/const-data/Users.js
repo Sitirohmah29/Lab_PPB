e8abf0b41a3179db1bdf7aaff073eb87fda1f0b8
Siti Rohmah <113085972+Sitirohmah29@users.noreply.github.com>
2024-04-24 16:38:49 +0700

    Add files via upload

---
+++ src/components/const-data/Users.js
@@ -0,0 +1,14 @@
+const Users = [
+	{ id: 1, name: "Rayyan", fullname: "Rayyan Pratama", gender: "M" },
+	{ id: 2, name: "Naila", fullname: "Naila Martiza", gender: "F" },
+	{ id: 3, name: "Rhmh", fullname: "Siti Rohmah", gender: "F" },
+	{ id: 4, name: "Sophie", fullname: "Sophie Makianoor", gender: "F" },
+	{ id: 5, name: "Nurul", fullname: "Nurul Fikri", gender: "M" },
+	{ id: 6, name: "Sandi", fullname: "Sandi Maulana", gender: "M" },
+	{ id: 7, name: "Andin", fullname: "Sandrina", gender: "F" },
+	{ id: 8, name: "Risma", fullname: "Risma Handayani", gender: "F" },
+	{ id: 9, name: "Rayyan", fullname: "Rayyan Pratama", gender: "M" },
+	{ id: 10, name: "Rayyan", fullname: "Rayyan Pratama", gender: "M" },
+];
+
+export { Users };
